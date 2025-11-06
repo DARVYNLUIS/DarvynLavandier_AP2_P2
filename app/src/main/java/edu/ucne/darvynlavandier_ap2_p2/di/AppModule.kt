d0e33ed4da529c574129a5d527fbe7a0aca12ef6
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-11-05 21:43:17 -0400

    APP MODULE.

---
+++ app/src/main/java/edu/ucne/darvynlavandier_ap2_p2/di/AppModule.kt
@@ -0,0 +1,20 @@
+package edu.ucne.darvynlavandier_ap2_p2.di
+
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.darvynlavandier_ap2_p2.data.repository.GastoRepositoryImpl
+import edu.ucne.darvynlavandier_ap2_p2.domain.repository.GastoRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+    @Binds
+    @Singleton
+    abstract fun bindGastoRepository(
+        impl: GastoRepositoryImpl
+    ): GastoRepository
+}
